2635f9cc0ff290a699d12953d5b74bb7262a1df4
naflct <94752153+naflct@users.noreply.github.com>
2022-03-28 21:55:52 +0300

    Add files via upload

---
+++ dz2_flita.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int main() {
+  int n;
+  int A[101][101];
+  FILE *graph1, *graph2;
+  graph1 = fopen("graph1.txt", "r");
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      fscanf(graph1, "%d", &A[i][j]);
+    }
+  }
+  fclose(graph1);
+
+  graph2 = fopen("graph2.txt", "w");
+  fprintf(graph2, "graph {");
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (A[i][j] != 0)
+      {
+        fprintf(graph2, "%d", i + 1);
+        fprintf(graph2, " -- ");
+        fprintf(graph2, "%d\n", j + 1);
+        if (A[i][j] > 1)
+        {
+          fprintf(graph2, " [label = %d", A[i][j]);
+          fprintf(graph2, " ]\n ");
+        }
+      }
+    }
+  }
+  fprintf(graph1, " } ");
+}
